8a18d66ba6b0c1a505c24b705bd1f75d0aed54eb
@zimeg <zim@o526.net>
2024-07-25 12:35:10 -0700

    feat: introduce a socket mode sample that responds with greetings

---
+++ README.md
@@ -0,0 +1,12 @@
+# js.bolt.socket
+
+Herewithin is an app that connects via Socket Mode and responds to greetings.
+
+```sh
+$ slack create socket -t zimeg/slacks -b js.bolt.socket
+$ cd socket
+$ slack run      # Development
+$ slack install  # Production
+```
+
+Visit App Config to inspect installation values: https://api.slack.com/apps
---
+++ app.js
@@ -0,0 +1,21 @@
+import bolt from "@slack/bolt";
+
+const app = new bolt.App({
+  token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  logLevel: bolt.LogLevel.DEBUG,
+});
+
+app.message("hello", async ({ say }) => {
+  say("howdy");
+});
+
+(async () => {
+  try {
+    await app.start();
+    console.log("⚡️ Bolt app is running!");
+  } catch (error) {
+    console.error("Failed to start the app", error);
+  }
+})();
